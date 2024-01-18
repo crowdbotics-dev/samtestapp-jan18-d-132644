38cfb63e8b93cb138e0e81674e8e0980e48c1271
Crowdbotics <engineering@crowdbotics.com>
2024-01-18 14:37:55 +0000

    Initial commit

---
+++ backend/samtestapp_jan18_d_132644/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for samtestapp_jan18_d_132644 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "samtestapp_jan18_d_132644.settings"
+)
+
+application = get_wsgi_application()
